92281a8d356df32b7580d5d3fc0cb55d88d18049
limminho94 <dla47941343@gmail.com>
2024-08-17 19:29:53 +0900

    트리만들기 까지

--- p11.c
+++ p11.c
@@ -14,7 +14,7 @@ void wood()
     {
         for(j=1;j<=5-i;j++)
         {
-            printf("%2s", "d");
+            printf("%2s", " ");
         }
         for(j=2;j<=i;j++)
         {
@@ -30,11 +30,43 @@ void wood()
     {
         for(j=1;j<=4-i;j++)
         {
-            printf("%2s", "d");
+            printf("%2s", " ");
         }
         for(j=1;j<=i;j++)
         {
             printf("%2s", "*");
+        }
+        for(j=1;j<=i+1;j++)
+        {
+            printf("%2s","*");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=3;i++)
+    {
+        for(j=1;j<=3-i;j++)
+        {
+            printf("%2s", " ");
+        }
+        for(j=1;j<=i+1;j++)
+        {
+            printf("%2s", "*");
+        }
+        for(j=1;j<=i+2;j++)
+        {
+            printf("%2s","*");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=3;i++)
+    {
+        for(j=1;j<=2;j++)
+        {
+            printf("%2s", " ");
+        }
+        for(j=1;j<=5;j++)
+        {
+            printf("%2s","*");
         }
         printf("\n");
     }
